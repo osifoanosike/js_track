26dcd0d4fd5f1101c5e92390ccf2a95c1560964c
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2014-12-20 09:12:40 +0100

    Exercise 2 -- changes made according to feedbacks

--- exercise_2.js
+++ exercise_2.js
@@ -1,11 +1,13 @@
 
-function CheckAll(className) {
-	this.className = className;
+function ToggleCheck(className) {
+  this.className = className;
 }
 
-CheckAll.prototype.check = function(checked) {
-	var checkboxes = document.getElementsByClassName(this.className);
-    for (i = 0; i < checkboxes.length; i++) {
-    	checkboxes[i].checked = checked;
-    }
+ToggleCheck.prototype.check = function(checkStatus) {
+  var checkboxes = document.getElementsByClassName(this.className);
+  for (i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].checked = checkStatus;
+  }
 }
+
+var toggle = new ToggleCheck('colors');
