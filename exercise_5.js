6854387278d1d64e035b8d38aefbf499a9e3a974
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2014-12-20 09:56:56 +0100

    Exercise 5 (Dynamic Table add rows, edit and delete existing rows)

--- exercise_5.js
+++ exercise_5.js
@@ -1,40 +1,45 @@
-var rowCount = 0;
 
-    function addRow() {
-      rowCount++;
-      var rowNumber = rowCount.toString();
-      var tbody = document.getElementById('tbody');
-      var tr = document.createElement('tr');
+function SubmitForm() {
+  this.rowCount = 0;
+}
 
-      tbody.appendChild(tr);
+SubmitForm.prototype.addRow = function() {
+  //rowCount = this.rowCount;
+  this.rowCount++;
+  var rowNumber = this.rowCount.toString();
+  var tbody = document.getElementById('tbody');
+  var tr = document.createElement('tr');
+
+  tbody.appendChild(tr);
       
-      tr.innerHTML = '<td class="' + rowNumber + '"><input type="text"></td>'
-                      + '<td class="' + rowNumber + '"><input type="text"></td>'
-                      + '<td><button id="' + rowNumber + '">Submit</button></td>';
+  tr.innerHTML = '<td class="' + rowNumber + '"><input type="text"></td>'
+                  + '<td class="' + rowNumber + '"><input type="text"></td>'
+                  + '<td><button id="' + rowNumber + '">Submit</button></td>';
       
-      var btn = document.getElementById(rowNumber);
-      btn.setAttribute('onclick', "submit('" + rowNumber + "')");
+  var btn = document.getElementById(rowNumber);
+  btn.setAttribute('onclick', "submit('" + rowNumber + "')");
+};
+
+var submit = function(rowNumber) {
+  var inputs = document.getElementsByClassName(rowNumber);
+
+  var name = inputs[0].firstChild.value;
+  var email = inputs[1].firstChild.value;
+      
+  if (notEmpty(name) && notEmpty(email)) {
+    if (!validate(inputs[1].firstChild.value)) {
+      return false;
+    } else {
+      inputs[0].innerHTML = name;
+      inputs[1].innerHTML = email;
+      inputs[1].nextSibling.innerHTML = '<a onclick="editRow(' + rowNumber + ')" href="#">Edit</a>'
+                                        + ' / <a onclick="removeRow(' + rowNumber + ')" href="#">Delete</a>';          
     }
-
-    function submit(rowNumber) {
-      var inputs = document.getElementsByClassName(rowNumber);
-
-      var name = inputs[0].firstChild.value;
-      var email = inputs[1].firstChild.value;
-      
-      if (notEmpty(name) && notEmpty(email)) {
-        if (!validate(inputs[1].firstChild.value)) {
-          return false;
-        } else {
-          inputs[0].innerHTML = name;
-          inputs[1].innerHTML = email;
-          inputs[1].nextSibling.innerHTML = '<a onclick="editRow(' + rowNumber + ')" href="#">Edit</a>'
-                                            + ' / <a onclick="removeRow(' + rowNumber + ')" href="#">Delete</a>';          
-        }
-      } else {
-        alert('Make sure there are no empty fields.');
-      }
-    }
+  } else {
+    alert('Make sure there are no empty fields.');
+  }
+};
+    
 
     function validate(email) {
       var atpos = email.indexOf("@");
@@ -54,19 +59,21 @@ var rowCount = 0;
       }
     }
 
-    function editRow(rowNumber) {
-      var tdValues = document.getElementsByClassName(rowNumber);
-      for (var i = 0; i < tdValues.length; i++) {
-        inputValue = tdValues[i].innerHTML;
-        tdValues[i].innerHTML = '<td><input value="' + inputValue + '" type="text"></td>'
-      }
-      tdValues[1].nextSibling.innerHTML = '<td><button id="' + rowNumber + '">Submit</button></td>';
-      var btn = document.getElementById(rowNumber);
-      btn.setAttribute('onclick', "submit('" + rowNumber + "')");
-    }
+var editRow = function(rowNumber) {
+  var tdValues = document.getElementsByClassName(rowNumber);
+  for (var i = 0; i < tdValues.length; i++) {
+    inputValue = tdValues[i].innerHTML;
+    tdValues[i].innerHTML = '<td><input value="' + inputValue + '" type="text"></td>'
+  }
+  tdValues[1].nextSibling.innerHTML = '<td><button id="' + rowNumber + '">Submit</button></td>';
+  var btn = document.getElementById(rowNumber);
+  btn.setAttribute('onclick', "submit('" + rowNumber + "')");
+}
+    
+var removeRow = function(rowNumber) {
+  parent = document.getElementById('tbody');
+  child = document.getElementsByClassName(rowNumber)[0].parentNode;
+  parent.removeChild(child);
+};
 
-    function removeRow(rowNumber) {
-      parent = document.getElementById('tbody');
-      child = document.getElementsByClassName(rowNumber)[0].parentNode;
-      parent.removeChild(child);
-    }
+var submitIt = new SubmitForm();
