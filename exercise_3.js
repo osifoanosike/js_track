d172ff5fd39d677d31b6f61ba842024bc89f96af
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2014-12-23 12:56:53 +0100

    Exercise 3 -- changes made according to feedbacks

--- exercise_3.js
+++ exercise_3.js
@@ -1,60 +1,55 @@
 
 function CheckLimit(className, limit) {
-    this.className = className;
-    this.limit  = limit;
-    this.count = 0;
+  this.className = className;
+  this.limit  = limit;
+  this.count = 0;
 }
 
 CheckLimit.prototype.check = function(id) {
-    
-    var none = document.getElementById('none');
-    none.checked = false;
-    var checkbox = document.getElementById(id);
+  
+  var none = document.getElementById('none');
+  none.checked = false;
+  var checkbox = document.getElementById(id);
 
-    if (checkbox.checked) {
-        if (this.count < this.limit) {
-            this.count++
-        } else {
-            checkbox.checked = false;
-            var checkboxes = document.getElementsByClassName(this.className);
-            var checkedValue = [];
-            var len = checkboxes.length;
-            var and = len - 1;
-            var message = 'Only ' + this.limit + ' days can be selected. You have already selected ';
-            
-            for (var i = 0; i < len; i++) {
-                if (checkboxes[i].checked) {
-                    checkedValue.push(checkboxes[i].value);
-                }
-            }
-
-            for (var i = 0, len = checkedValue.length; i < len; i++) {
-                if (i == 0) {
-                    message += checkedValue[i];
-                } else if (i == len - 1) {
-                    message += ' and ';
-                    message += checkedValue[i];
-                    message += '.';
-                } else {
-                    message += ', ';
-                    message += checkedValue[i];
-                }
-            }
-            alert(message);
-        }
+  if (checkbox.checked) {
+    if (this.count < this.limit) {
+      this.count++
     } else {
-        this.count--
+      checkbox.checked = false;
+      var className = '.' + this.className;
+
+      var checkboxes = document.querySelectorAll(className);
+      var checkedValue = [];
+      var len = checkboxes.length;
+      var and = len - 1;
+      
+      for (var i = 0; i < len; i++) {
+        if (checkboxes[i].checked) {
+          checkedValue.push(checkboxes[i].value);
+        }
+      }
+
+      var message = 'Only ' + this.limit + ' days can be selected. You have already selected '
+                      + checkedValue[0] + ', ' + checkedValue[1] + ' and ' + checkedValue[2] + '.';
+      alert(message);
     }
+  } else {
+    this.count--
+  }
 };
 
 CheckLimit.prototype.uncheck = function() {
-    
-    var none = document.getElementById('none');
+  
+  var none = document.getElementById('none');
+  var className = '.' + this.className;
 
-    var checkboxes = document.getElementsByClassName(this.className);
-    for (i = 0; i < checkboxes.length; i++) {
-        checkboxes[i].checked = false;
-    }
+  var checkboxes = document.querySelectorAll(className);
+  var len = checkboxes.length;
+  for (i = 0; i < len; i++) {
+    checkboxes[i].checked = false;
+  }
 
-    this.count = 0;
+  this.count = 0;
 };
+
+var checkLimit = new CheckLimit('days', 3);
