f9785878931fe0aed9e0c56adc85b4b433746db0
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2014-12-29 08:36:05 +0100

    Exercise 11 (Matching numerical values and event propogation)

--- exercise_11.js
+++ exercise_11.js
@@ -2,20 +2,21 @@
 function CheckNumeric() {
 }
 
-CheckNumeric.prototype.checkInput = function() {
-  var form = document.forms['form'];
+CheckNumeric.prototype.checkInput = function(formName) {
+  var form = document.forms[formName];
   var input = form.number.value.trim();
 
   if (empty(input)) {
-    alert('The Number field cannot be empty');
+    alert('The Number field cannot be empty.');
     return;
   }
 
   if (isNaN(input)) {
     form.result.value = 'False';
+    alert('False! The input is not a number.')
   } else {
     form.result.value = 'True';
-    alert('Click OK and wait for 10 seconds for the form to submit.');
+    alert('True. The input is a number.\n\nClick OK and wait for 10 seconds for the form to submit.');
     setTimeout(function(){form.submit()},10000);
   }
 }
@@ -32,4 +33,4 @@ CheckNumeric.prototype.checkInput = function() {
     }
   }
 
-var matchNumber = new CheckNumeric;
+var matchNumber = new CheckNumeric();
