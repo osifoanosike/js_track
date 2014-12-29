dbf6a65f51585d2561ce8d2b4ec288d86a59645b
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2014-12-29 19:16:11 +0100

    Exercise 6 -- Form validation using js

---
+++ Ex6/exercise_6.js
@@ -0,0 +1,46 @@
+
+function Validation() {
+
+}
+
+Validation.prototype.checkValue = function(formName) {
+
+  var form = document.forms[formName];
+
+  if (empty(form.loginid.value)) {
+    message = "Login Id can't be empty.";
+  } else if (empty(form.email.value)) {
+    message = "Email can't be empty.";
+  } else if (empty(form.name.value)) {
+    message = "Name can't be empty.";
+  } else if (empty(form.timezone.value)) {
+    message = "Timezone can't be empty.";
+  } else if (empty(form.homepage.value)) {
+    message = "Homepage can't be empty.";
+  } else if (empty(form.about.value)) {
+    message = "About me can't be empty.";
+  } else if (form.about.value.length < 50) {
+    message = 'About me section must have at least 50 characters.';
+  } else {
+    message = '';
+  }
+
+  if (message) {
+    alert(message);
+    event.preventDefault();
+  }
+}
+
+function empty(value) {
+  if (value !== null) {
+    value = value.trim();
+  }
+
+  if (!value) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var validator = new Validation();
