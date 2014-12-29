3890774373cfb8a6c473bfd6a95ab0993b7faddf
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2014-12-29 19:13:13 +0100

    Exercise 2 -- Check all or none

---
+++ Ex2/exercise_2.js
@@ -0,0 +1,13 @@
+
+function ToggleCheck(className) {
+  this.className = className;
+}
+
+ToggleCheck.prototype.check = function(checkStatus) {
+  var checkboxes = document.getElementsByClassName(this.className);
+  for (i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].checked = checkStatus;
+  }
+}
+
+var toggle = new ToggleCheck('colors');
