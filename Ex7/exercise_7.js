363499c3cfaf6f93369c551e3f21f5c621abefa9
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2014-12-31 13:45:15 +0100

    Exercise 7 -- Name input in Alert and Welcome in page content

--- Ex7/exercise_7.js
+++ Ex7/exercise_7.js
@@ -1,34 +1,21 @@
 
 function Account() {
-  
 }
 
 Account.prototype.getName = function() {
   do {
     var firstName = prompt('What is your first name');
-  } while (empty(firstName))
+  } while (validator.empty(firstName))
 
   do {
     var lastName = prompt('What is your last name');
-  } while (empty(lastName))
+  } while (validator.empty(lastName))
 
   var message = 'Hello ' + firstName + ' ' + lastName + '.';
 
   alert(message);
 
   document.write('<strong>' + message + '</strong>');
-}
-
-function empty(value) {
-  if (value !== null) {
-    value = value.trim();
-  }
-
-  if (!value) {
-    return true;
-  } else {
-    return false;
-  }
 }
 
 var owner = new Account();
