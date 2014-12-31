d736a59631af94549e30bd5cca3d6f8b2ce4fd30
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2014-12-31 10:23:14 +0100

    Removed innerHTML

--- Ex5/Table.js
+++ Ex5/Table.js
@@ -9,15 +9,35 @@ Table.prototype.addRow = function() {
   var rowNumber = this.rowCount.toString();
   var tbody = document.getElementById('tbody');
   var tr = document.createElement('tr');
-
   tbody.appendChild(tr);
-      
-  tr.innerHTML = '<td class="' + rowNumber + '"><input type="text"></td>'
-                  + '<td class="' + rowNumber + '"><input type="text"></td>'
-                  + '<td><button id="' + rowNumber + '">Submit</button></td>';
-      
-  var btn = document.getElementById(rowNumber);
-  btn.setAttribute('onclick', "submit('" + rowNumber + "')");
+
+  var tdName = document.createElement('td');
+  var tdEmail = document.createElement('td');
+  var tdAction = document.createElement('td');
+
+  var inputName = document.createElement('input');
+  var inputEmail = document.createElement('input');
+
+  inputName.type = 'text';
+  inputEmail.type = 'text';
+
+  var button = document.createElement('button');
+  button.id = rowNumber;
+  button.setAttribute('onclick', "submit('" + rowNumber + "')");
+
+  var submit = document.createTextNode('Submit');
+  button.appendChild(submit);
+
+  tdName.className = rowNumber;
+  tdEmail.className = rowNumber;
+
+  tdName.appendChild(inputName);
+  tdEmail.appendChild(inputEmail);
+  tdAction.appendChild(button);
+
+  tr.appendChild(tdName);
+  tr.appendChild(tdEmail);
+  tr.appendChild(tdAction);
 }
 
 var submit = function(rowNumber) {
@@ -25,15 +45,36 @@ var submit = function(rowNumber) {
 
   var name = inputs[0].firstChild.value;
   var email = inputs[1].firstChild.value;
+
+  var nameNode = document.createTextNode(name);
+  var emailNode = document.createTextNode(email);
+
+  var editLink = document.createElement('a');
+  var editText = document.createTextNode('Edit');
+  editLink.appendChild(editText);
+  editLink.setAttribute('onclick', 'editRow(' + rowNumber + ')');
+  editLink.href = '#';
+
+  var deleteLink = document.createElement('a');
+  var deleteText = document.createTextNode('Delete');
+  deleteLink.appendChild(deleteText);
+  deleteLink.setAttribute('onclick', 'deleteRow(' + rowNumber + ')');
+  deleteLink.href = '#';
+
+  var separator = document.createTextNode(' / ');
+
+  var tdAction = inputs[1].nextSibling
       
   if (!validator.empty(name) && !validator.empty(email)) {
     if (!validator.validate(email)) {
       return false;
     } else {
-      inputs[0].innerHTML = name;
-      inputs[1].innerHTML = email;
-      inputs[1].nextSibling.innerHTML = '<a onclick="editRow(' + rowNumber + ')" href="#">Edit</a>'
-                                        + ' / <a onclick="removeRow(' + rowNumber + ')" href="#">Delete</a>';          
+      inputs[0].replaceChild(nameNode, inputs[0].firstChild);
+      inputs[1].replaceChild(emailNode, inputs[1].firstChild);
+
+      tdAction.replaceChild(editLink, inputs[1].nextSibling.firstChild);
+      tdAction.appendChild(separator);
+      tdAction.appendChild(deleteLink);         
     }
   } else {
     alert('Make sure there are no empty fields.');
@@ -44,14 +85,26 @@ var editRow = function(rowNumber) {
   var tdValues = document.getElementsByClassName(rowNumber);
   for (var i = 0; i < tdValues.length; i++) {
     inputValue = tdValues[i].innerHTML;
-    tdValues[i].innerHTML = '<td><input value="' + inputValue + '" type="text"></td>'
+    input = document.createElement('input');
+    input.value = inputValue;
+    input.type = 'text';
+    tdValues[i].replaceChild(input, tdValues[i].firstChild);
   }
-  tdValues[1].nextSibling.innerHTML = '<td><button id="' + rowNumber + '">Submit</button></td>';
-  var btn = document.getElementById(rowNumber);
-  btn.setAttribute('onclick', "submit('" + rowNumber + "')");
+
+  var tdAction = tdValues[1].nextSibling;
+  var button = document.createElement('button');
+  var text = document.createTextNode('Submit');
+
+  button.id = rowNumber;
+  button.setAttribute('onclick', "submit('" + rowNumber + "')");
+  button.appendChild(text);
+
+  tdAction.replaceChild(button, tdAction.firstChild);
+  tdAction.replaceChild(button, tdAction.childNodes[1])
+  tdAction.replaceChild(button, tdAction.lastChild);
 }
     
-var removeRow = function(rowNumber) {
+var deleteRow = function(rowNumber) {
   parent = document.getElementById('tbody');
   child = document.getElementsByClassName(rowNumber)[0].parentNode;
   parent.removeChild(child);
