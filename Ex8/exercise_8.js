c546e36c50e33defc248e79645f584a993c52849
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-01-05 14:55:54 +0100

    Exercise 8 -- Input URL and open it

--- Ex8/exercise_8.js
+++ Ex8/exercise_8.js
@@ -3,16 +3,23 @@ function UrlOpener() {
 }
 
 UrlOpener.prototype.openUrl = function() {
-  do {
-    var address = prompt('Please enter the web address.');
-  } while (validator.empty(address));
 
-  var windowFeatures = 'menubar=no,location=no,status=no,scrollbars=0,width=400,height=450';
-  var url = 'http://' + address;
+  window.addEventListener('load', function() {
 
-  alert('You would like to visit ' + url);
+    do {
+      var address = prompt('Please enter the web address.');
+    } while (validator.empty(address));
 
-  window.open(url, '_blank', windowFeatures);
+    var windowFeatures = 'menubar=no,location=no,status=no,scrollbars=0,width=400,height=450';
+    var url = 'http://' + address;
+
+    alert('You would like to visit ' + url);
+
+    window.open(url, '_blank', windowFeatures);
+
+  }, false);
+
 }
 
 var opener = new UrlOpener();
+opener.openUrl();
