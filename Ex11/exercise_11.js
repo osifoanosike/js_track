5009f07d2da5204764e3509e1f53a479712e1459
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-01-13 16:26:51 +0100

    Add event listener to the submit button.

--- Ex11/exercise_11.js
+++ Ex11/exercise_11.js
@@ -3,22 +3,34 @@ function CheckNumeric() {
 }
 
 CheckNumeric.prototype.checkInput = function(formName) {
-  var form = document.forms[formName];
-  var input = form.number.value.trim();
+  
+  window.addEventListener('load', function() {
 
-  if (validator.empty(input)) {
-    alert('The Number field cannot be empty.');
-    return;
-  }
+    var button = document.getElementById('submit');
+    
+    button.addEventListener('click', function() {
+      
+      var form = document.forms[formName];
+      var input = form.number.value.trim();
 
-  if (isNaN(input)) {
-    form.result.value = 'False';
-    alert('False! The input is not a number.')
-  } else {
-    form.result.value = 'True';
-    alert('True. The input is a number.\n\nClick OK and wait for 10 seconds for the form to submit.');
-    setTimeout(function(){form.submit()},10000);
-  }
+      if (validator.empty(input)) {
+        alert('The Number field cannot be empty.');
+        return;
+      }
+
+      if (isNaN(input)) {
+        form.result.value = 'False';
+        alert('False! The input is not a number.')
+      } else {
+        form.result.value = 'True';
+        alert('True. The input is a number.\n\nClick OK and wait for 10 seconds for the form to submit.');
+        setTimeout(function(){form.submit()},10000);
+      }
+
+    });
+
+  });
 }
 
 var matchNumber = new CheckNumeric();
+matchNumber.checkInput('form');
