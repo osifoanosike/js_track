07b14fb5fee67915eb5a811162c1c1e44e9c1917
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-01-20 12:02:40 +0100

    Create new methods for input valiation.

--- Ex11/exercise_11.js
+++ Ex11/exercise_11.js
@@ -1,36 +1,61 @@
 
-function CheckNumeric() {
+function NumberCheck() {
 }
 
-CheckNumeric.prototype.checkInput = function(formName) {
+NumberCheck.prototype.checkNumber = function(formId) {
+
+  var that = this;
   
   window.addEventListener('load', function() {
 
     var button = document.getElementById('submit');
     
-      button.addEventListener('click', function() {
+    button.addEventListener('click', function() {
       
-      var form = document.forms[formName];
-      var input = form.number.value.trim();
+      var form = document.getElementById(formId);
+      var input = document.getElementById('number');
+      var number = input.value.trim();
 
-      if (validator.empty(input)) {
-        alert('The Number field cannot be empty.');
-        return;
-      }
-
-      if (isNaN(input)) {
-        form.result.value = 'False';
-        alert('False! The input is not a number.')
-      } else {
-        form.result.value = 'True';
-        alert('True. The input is a number.\n\nClick OK and wait for 10 seconds for the form to submit.');
-        setTimeout(function(){form.submit()},10000);
-      }
-
+      that.validateInput(number);
     });
 
   });
 }
 
-var matchNumber = new CheckNumeric();
-matchNumber.checkInput('form');
+NumberCheck.prototype.validateInput = function(input) {
+
+  if (this.isEmpty(input)) {
+    return;
+  } else {
+    this.isNumber(input);
+  }
+
+}
+
+NumberCheck.prototype.isEmpty = function(input) {
+  if (validator.empty(input)) {
+    alert('The Number field cannot be empty.');
+    return true;
+  }
+}
+
+NumberCheck.prototype.isNumber = function(input) {
+
+  var regexNumeral = /^[0-9]+$/;
+  var result = document.getElementById('result');
+
+  if (regexNumeral.test(input)) {
+    result.value = 'True';
+    alert('True! The input is a number.\n\nClick OK and wait for 5 seconds for the form to submit.');
+    setTimeout(function() {
+      form.submit()
+    }, 5000);
+
+  } else {
+    result.value = 'False';
+    alert('False! The input is not a number.');
+  }
+}
+
+var matchNumber = new NumberCheck();
+matchNumber.checkNumber('form');
