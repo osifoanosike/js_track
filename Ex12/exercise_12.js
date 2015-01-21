1aff7425160c430578ba85ca8c4f1cf037dc3e36
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-01-21 18:27:11 +0100

    Move load event handler called on window outside prototype.

--- Ex12/exercise_12.js
+++ Ex12/exercise_12.js
@@ -4,49 +4,50 @@ function ExtractUrl() {
 
 ExtractUrl.prototype.extract = function(formName) {
 
-  window.addEventListener('load', function() {
+  var button = document.getElementById('submit');
 
-    button = document.getElementById('submit');
+  button.addEventListener('click', function() {
+    
+    var form = document.forms[formName];
+    var url = form.url.value;
 
-    button.addEventListener('click', function() {
-      
-      var form = document.forms[formName];
-      var url = form.url.value;
+    var regex = /(\/\/)[a-z0-9.-]+\//g;
+    var regex2 = /[a-z0-9.-]/g;
+    
+    var extract = url.match(regex);
 
-      var regex = /(\/\/)[a-z0-9.-]+\//g;
-      var regex2 = /[a-z0-9.-]/g;
-      
-      var extract = url.match(regex);
+    var webAddress = extract[0].match(regex2).join('');
 
-      var webAddress = extract[0].match(regex2).join('');
+    var webArray = webAddress.split('.');
 
-      var webArray = webAddress.split('.');
+    if (webAddress.match(/co(?=\.)/)) {
 
-      if (webAddress.match(/co(?=\.)/)) {
-
-        if (webArray.length == 3) {
-          alert('Domain: ' + webAddress);
-        } else {
-          alert('Domain: ' + webArray[1] + '.' + webArray[2] + '.' + webArray[3] + '\n\n'
-                + 'Subdomain: ' + webArray[0]
-          );
-        }
-
+      if (webArray.length == 3) {
+        alert('Domain: ' + webAddress);
       } else {
-
-        if (webArray.length == 2) {
-          alert('Domain: ' + webAddress);
-        } else {
-          alert('Domain: ' + webArray[1] + '.' + webArray[2] + '\n\n'
-                + 'Subdomain: ' + webArray[0]
-          );
-        }
+        alert('Domain: ' + webArray[1] + '.' + webArray[2] + '.' + webArray[3] + '\n\n'
+              + 'Subdomain: ' + webArray[0]
+        );
       }
-    })
 
+    } else {
+
+      if (webArray.length == 2) {
+        alert('Domain: ' + webAddress);
+      } else {
+        alert('Domain: ' + webArray[1] + '.' + webArray[2] + '\n\n'
+              + 'Subdomain: ' + webArray[0]
+        );
+      }
+    }
   });
 
 }
 
-var extractUrl = new ExtractUrl();
-extractUrl.extract('form');
+
+window.addEventListener('load', function() {
+  
+  var extractUrl = new ExtractUrl();
+  extractUrl.extract('form');
+
+});
