5c2e65584b0832b57114bf73b5fb8b3721c7a84c
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-01-22 12:24:37 +0100

    Move load event handler called on window outside prototype.

--- Ex6/exercise_6.js
+++ Ex6/exercise_6.js
@@ -4,38 +4,34 @@ function Validation() {
 
 Validation.prototype.checkValue = function(formName) {
 
-  window.addEventListener('load', function() {
+  var form = document.forms[formName];
 
-    var form = document.forms[formName];
+  form.addEventListener('submit', function() {
 
-    form.addEventListener('submit', function() {
+    var details = document.querySelectorAll('.details');
+    var message = "";
 
-      var details = document.querySelectorAll('.details');
-      var message = "";
+    for (var i = 0; i < details.length; i++) {
 
-      for (var i = 0; i < details.length; i++) {
-
-        if (validator.empty(details[i].value)) {
-          var labelText = details[i].previousElementSibling.textContent;
-          message = labelText + ' cannot be empty.';
-          alert(message);
-        }
-      }
-
-      if (validator.validateMinLength(form.about.value, 50)) {
-        message = 'About Me must have at least 50 characters.';
+      if (validator.empty(details[i].value)) {
+        var labelText = details[i].previousElementSibling.textContent;
+        message = labelText + ' cannot be empty.';
         alert(message);
       }
+    }
 
-      if (message) {
-        event.preventDefault();
-      }
+    if (validator.validateMinLength(form.about.value, 50)) {
+      message = 'About Me must have at least 50 characters.';
+      alert(message);
+    }
 
-    });
-
+    if (message) {
+      event.preventDefault();
+    }
   });
-
 }
 
-var validation = new Validation();
-validation.checkValue('form');
+window.addEventListener('load', function() {
+  var validation = new Validation();
+  validation.checkValue('form');
+});
