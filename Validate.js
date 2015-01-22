871e6b5be94a371ea5ca163cdc2d7bb7b79de2cd
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-01-22 11:33:20 +0100

    Change regex for url validation to cater for www.

--- Validate.js
+++ Validate.js
@@ -17,7 +17,7 @@ Validate.prototype.empty = function(value) {
 
 Validate.prototype.validateEmail = function(url) {
   
-  var emailRegExp = /^[a-z0-9.-_]+@[a-z0-9]{2,}.[a-z.]{2,6}/;
+  var emailRegExp = /^[a-z0-9.-_]+@[a-z0-9]{2,}.[a-z.]{2,6}/i;
   if (emailRegExp.test(url)) {
     return true;
   }
@@ -26,12 +26,14 @@ Validate.prototype.validateEmail = function(url) {
 
 Validate.prototype.validateUrl = function(url) {
   //var urlRegExp = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/i;
+  // ^((?:https?|ftp):\/\/)[a-z0-9.-]+\/[a-z0-9.]+
+  // ^((?:https?|ftp):\/\/)[a-z0-9.-]+\/?([a-z0-9.]+\/?)+
   
-  var urlRegExp = /^((?:https?|ftp):\/\/)[a-z0-9.-]+\/[a-z0-9.]+/;
-  if (urlRegExp.test(url)) {
-    return true;
-  }
-  return false;
+  var urlRegExp = /^(https?:\/\/|ftp:\/\/|www\.)[a-z0-9.-]+\/?([a-z0-9.]+\/?)+/i;
+    if (urlRegExp.test(url)) {
+      return true;
+    }
+    return false;
 }
 
 Validate.prototype.validateMinLength = function(value, minLength) {
