c053b0da9fac3a65c7a70dfe18b21db6061fa3d4
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-01-26 09:09:27 +0100

    Move addRow() method to Row class. Create an init() method.

--- Ex5/Table.js
+++ Ex5/Table.js
@@ -1,44 +1,16 @@
 
 function Table() {
-  this.rowCount = 0;
+  this.row;
 }
 
 Table.prototype.init = function() {
   
   that = this;
   var addButton = document.getElementById('add');
-    addButton.addEventListener('click', function() {
-    that.addRow();
+  addButton.addEventListener('click', function() {
+    that.row = new Row();
+    that.row.addRow();
   });
-}
-
-Table.prototype.addRow = function() {
-
-  that.rowCount++;
-  var rowNumber = that.rowCount.toString();
-
-  var tdName = cell.createTableData(rowNumber);
-  var tdEmail = cell.createTableData(rowNumber);
-
-  var tdAction = document.createElement('td');
-
-  var button = cell.createButton('Submit', rowNumber);
-
-  tdAction.appendChild(button);
-
-  var tr = document.createElement('tr');
-  that.append(tr, tdName, tdEmail, tdAction);
-
-  var tbody = document.getElementById('tbody');
-  tbody.appendChild(tr); 
-}
-    
-/** Use when appending muliple children node to a parent node */
-
-Table.prototype.append = function(parent, children) {
-  for (var i = 1; i < arguments.length; i++) {
-    parent.appendChild(arguments[i]);
-  }
 }
 
 window.addEventListener('load', function() {
