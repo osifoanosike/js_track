886d8992e90831ec9ebacd66896c6e91ac23add9
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-01-27 20:38:29 +0100

    Create an instance of the Row class with makeRow() method.

--- Ex5/table.js
+++ Ex5/table.js
@@ -1,14 +1,18 @@
 function Table() {
-  this.row;
 }
 
 Table.prototype.init = function() { 
   var that = this;
   var addButton = document.getElementById('add');
   addButton.addEventListener('click', function() {
-    that.row = new Row();
-    that.row.add();
+    that.makeRow();
   });
+}
+
+Table.prototype.makeRow = function() {
+  var row = new Row();
+  row.table = this;
+  row.add();
 }
 
 window.addEventListener('load', function() {
