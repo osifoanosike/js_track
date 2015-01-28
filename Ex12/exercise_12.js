ccda375e26ca401616e805ac83645d37d8525472
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-01-28 09:29:08 +0100

    Code clean up.

--- Ex12/exercise_12.js
+++ Ex12/exercise_12.js
@@ -1,34 +1,26 @@
-
 function Domain() {
   this.regex = /^(?:(?:https?:\/\/www\.|https?:\/\/|ftp:\/\/www\.|ftp:\/\/|www\.))?([a-z0-9]+(?:(?:\.|-)?[a-z0-9]+)+(?:\.[a-z]{2,6}))(?:\/?(?!\/)(?:[a-z0-9]+(?:(?:-|_)[a-z0-9]+)*)*)*(\.[a-z]+)*/i;
 }
 
-Domain.prototype.init = function() {
-  
+Domain.prototype.init = function() {  
   that = this;
   var button = document.getElementById('submit');
-
   button.addEventListener('click', function() {
     that.extractDomain();
   });
 }
 
 Domain.prototype.extractDomain = function() {
-
   var url = document.getElementById('url');
   var urlValue = url.value;
-
   var address = urlValue.replace(that.regex, '$1');
-
-  that.displayDomain(address);
+  this.displayDomain(address);
 }
 
 Domain.prototype.displayDomain = function(webAddress) {
-
   var webArray = webAddress.split('.');
 
   if (webAddress.match(/co(?=\.)/)) {
-
     if (webArray.length == 3) {
       alert('Domain: ' + webAddress);
     } else {
@@ -38,7 +30,6 @@ Domain.prototype.displayDomain = function(webAddress) {
     }
 
   } else {
-
     if (webArray.length == 2) {
       alert('Domain: ' + webAddress);
     } else {
