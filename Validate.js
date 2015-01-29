6bcd7ce0e95201171cac7d014a35759153a30a83
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-01-29 20:28:20 +0100

    Update the regex for url validation.

--- Validate.js
+++ Validate.js
@@ -1,5 +1,3 @@
-// The class for validating the inputs.
-
 function Validate() {
 }
 
@@ -7,7 +5,6 @@ Validate.prototype.empty = function(value) {
   if (value !== null) {
     value = value.trim();
   }
-
   if (!value) {
     return true;
   } else {
@@ -15,10 +12,9 @@ Validate.prototype.empty = function(value) {
   }
 }
 
-Validate.prototype.validateEmail = function(url) {
-  
+Validate.prototype.validateEmail = function(email) {  
   var emailRegExp = /^[a-z]+(?:[a-z0-9]+)*(?:(\.|-|_)[a-z0-9]+)*@[a-z]+(?:[a-z0-9]+)*(?:(\.|-|_)[a-z0-9]+)*(?:\.[a-z]{2,6}(?:\.[a-z]{2})*)$/i;
-  if (emailRegExp.test(url)) {
+  if (emailRegExp.test(email)) {
     return true;
   }
   return false;
@@ -28,12 +24,12 @@ Validate.prototype.validateUrl = function(url) {
   //var urlRegExp = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/i;
   // ^((?:https?|ftp):\/\/)[a-z0-9.-]+\/[a-z0-9.]+
   // ^((?:https?|ftp):\/\/)[a-z0-9.-]+\/?([a-z0-9.]+\/?)+
-  
-  var urlRegExp = /^(?:https?:\/\/|ftp:\/\/|www\.)?[a-z0-9]+(?:(?:\.|-)?[a-z0-9]+)+(?:\.[a-z]{2,6})(?:(?:\/?(?!\/)(?:[a-z0-9]+(?:(?:-|_)[a-z0-9]+)*)*)*$)/;
-    if (urlRegExp.test(url)) {
-      return true;
-    }
-    return false;
+
+  var urlRegExp = /^(?:https?:\/\/|ftp:\/\/|www\.)?[a-z0-9]+(?:(?:\.|-)?[a-z0-9]+)*(?:\.[a-z]{2,6})(?:(?:\/?(?!\/)(?:[a-z0-9]+(?:(?:-|_)[a-z0-9]+)*)*)*$)/;
+  if (urlRegExp.test(url)) {
+    return true;
+  }
+  return false;
 }
 
 Validate.prototype.validateMinLength = function(value, minLength) {
