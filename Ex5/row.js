93acadc5fc88438ddead341557319b020073b2dd
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-01-30 22:58:01 +0100

    Rename variables and methods.

--- Ex5/row.js
+++ Ex5/row.js
@@ -5,66 +5,65 @@ function Row() {
 Row.count = 0;
 
 Row.prototype.add = function() {
-
   that = this;
   Row.count++;
   var rowNumber = Row.count.toString();
   
   this.cell = new Cell();
-  var tdName = this.cell.createTableData(rowNumber);
-  var tdEmail = this.cell.createTableData(rowNumber);
-  var tdAction = document.createElement('td');
+  var nameCell = this.cell.createCell(rowNumber);
+  var emailCell = this.cell.createCell(rowNumber);
+  var actionCell = document.createElement('td');
 
   var button = this.cell.createButton('Submit', rowNumber);
   button.onclick = this.submit;
-  tdAction.appendChild(button);
-  var tr = document.createElement('tr');
-  this.cell.append(tr, tdName, tdEmail, tdAction);
-  var tbody = document.getElementById('tbody');
-  tbody.appendChild(tr); 
+  actionCell.appendChild(button);
+  var row = document.createElement('tr');
+  this.cell.append(row, nameCell, emailCell, actionCell);
+  var table = document.getElementById('tbody');
+  table.appendChild(row); 
 }
 
 Row.prototype.submit = function() {
   var id = this.id;
-  var inputs = document.getElementsByClassName(id);
-  var name = inputs[0].firstChild.value;
-  var email = inputs[1].firstChild.value;
-  var inputTexts = that.cell.getValues(inputs);
+  var inputCells = document.getElementsByClassName(id);
+  var name = inputCells[0].firstChild.value;
+  var email = inputCells[1].firstChild.value;
+  var inputValues = that.cell.getInputValues(inputCells);
   var separator = document.createTextNode(' / ');
-  var tdAction = inputs[1].nextSibling;
+  var actionCell = inputCells[1].nextSibling;
 
   if (that.cell.validateInputs(name, email)) {
     var editLink = that.cell.createLink('Edit', id);
     var deleteLink = that.cell.createLink('Delete', id);
     editLink.onclick = that.edit;
     deleteLink.onclick = that.delete;
-    that.cell.replaceField(inputs, inputTexts);
-    that.cell.removeChildNodes(tdAction);
-    that.cell.append(tdAction, editLink, separator, deleteLink);
+    that.cell.replaceInputField(inputCells, inputValues);
+    that.cell.removeChildNodes(actionCell);
+    that.cell.append(actionCell, editLink, separator, deleteLink);
   }
 }
 
 Row.prototype.edit = function() {
   var id = this.id;
-  var tdValues = document.getElementsByClassName(id);
-  for (var i = 0; i < tdValues.length; i++) {
-    var inputValue = tdValues[i].innerHTML;
+  var inputCells = document.getElementsByClassName(id);
+  for (var i = 0; i < inputCells.length; i++) {
+    var inputValue = inputCells[i].innerHTML;
     var input = document.createElement('input');
     input.value = inputValue;
     input.type = 'text';
-    tdValues[i].replaceChild(input, tdValues[i].firstChild);
+    inputCells[i].replaceChild(input, inputCells[i].firstChild);
   }
 
-  var tdAction = tdValues[1].nextSibling;
+  var actionCell = inputCells[1].nextSibling;
   var button = that.cell.createButton('Submit', id);
   button.onclick = that.submit;
-  that.cell.removeChildNodes(tdAction);
-  that.cell.append(tdAction, button);
+  that.cell.removeChildNodes(actionCell);
+  that.cell.append(actionCell, button);
 }
 
 Row.prototype.delete = function() {
   var id = this.id;
-  var parent = document.getElementById('tbody');
-  var child = document.getElementsByClassName(id)[0].parentNode;
-  parent.removeChild(child);
+  var table = document.getElementById('tbody');
+  var row = document.getElementsByClassName(id)[0].parentNode;
+  table.removeChild(row);
 }
