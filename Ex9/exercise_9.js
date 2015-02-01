1b83e6dbc948f004053096ee025562afdfb28c89
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-02-01 15:03:29 +0100

    Create document fragment.

--- Ex9/exercise_9.js
+++ Ex9/exercise_9.js
@@ -5,12 +5,14 @@ MoveCountry.prototype.move = function(sourceBoxId, destinationBoxId) {
   var doc = document;
   var sourceList = doc.getElementById(sourceBoxId);
   var destinationList = doc.getElementById(destinationBoxId);
+  var fragment = doc.createDocumentFragment();
 
   var selectedCountries = sourceList.selectedOptions;
   for (var i = 0; i < selectedCountries.length; i++) {
-    destinationList.appendChild(selectedCountries[i]);
+    fragment.appendChild(selectedCountries[i]);
     i--;
   }
+  destinationList.appendChild(fragment);
 }
 
 var moveCountry = new MoveCountry();
