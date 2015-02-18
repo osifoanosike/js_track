4274981b5605d858baacd7b97ead021e53f23255
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-02-18 11:31:13 +0100

    Add init() method.

--- Ex2/exercise_2.js
+++ Ex2/exercise_2.js
@@ -2,6 +2,19 @@ function ToggleCheck(className) {
   this.className = className;
 }
 
+ToggleCheck.prototype.init = function() {
+  var that = this;
+  var doc  = document;
+  var all  = doc.getElementById('all');
+  var none = doc.getElementById('none');
+  all.addEventListener('click', function() {
+    that.check(true);
+  });
+  none.addEventListener('click', function() {
+    that.check(false);
+  });
+}
+
 ToggleCheck.prototype.check = function(checkStatus) {
   var checkboxes = document.getElementsByClassName(this.className);
   for (i = 0; i < checkboxes.length; i++) {
@@ -9,4 +22,7 @@ ToggleCheck.prototype.check = function(checkStatus) {
   }
 }
 
-var toggle = new ToggleCheck('colors');
+window.addEventListener('load', function() {
+  var toggle = new ToggleCheck('colors');
+  toggle.init();
+});
