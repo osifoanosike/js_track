497ba643c6b74f2c47057cdf087690b90d77b7b0
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-02-18 10:42:55 +0100

    Add an init() method.

--- Ex10/exercise_10.js
+++ Ex10/exercise_10.js
@@ -1,40 +1,43 @@
 function Validation() {
 }
 
-Validation.prototype.checkValue = function(formName) {
-  window.addEventListener('load', function() {
-    var form = document.forms[formName];
-    var message;
-
-    form.addEventListener('submit', function() {
-      var details = document.querySelectorAll('.details');
-      var message = "";
-
-      for (var i = 0; i < details.length; i++) {
-        if (validator.empty(details[i].value)) {
-          var labelText = details[i].previousElementSibling.textContent;
-          message = labelText + ' cannot be empty.';
-          alert(message);
-        }
-      }
-      if (!validator.validateEmail(form.email.value)) {
-        message = 'Please enter a valid email address';
-        alert(message);
-      }
-      if (!validator.validateUrl(form.homepage.value)) {
-        message = 'Please enter a valid url.';
-        alert(message);
-      }
-      if (validator.validateMinLength(form.about.value, 50)) {
-        message = 'About me section must have at least 50 characters.';
-        alert(message);
-      }
-      if (message) {
-        event.preventDefault();
-      }
-    });
+Validation.prototype.init = function(formName) {
+  var that = this;
+  var form = document.forms[formName];
+  form.addEventListener('submit', function() {
+    that.checkValue(this)
   });
+};
+
+Validation.prototype.checkValue = function(form) {
+  var details = document.querySelectorAll('.details');
+  var message = "";
+
+  for (var i = 0; i < details.length; i++) {
+    if (validator.empty(details[i].value)) {
+      var labelText = details[i].previousElementSibling.textContent;
+      message = labelText + ' cannot be empty.';
+      alert(message);
+    }
+  }
+  if (!validator.validateEmail(form.email.value)) {
+    message = 'Please enter a valid email address';
+    alert(message);
+  }
+  if (!validator.validateUrl(form.homepage.value)) {
+    message = 'Please enter a valid url.';
+    alert(message);
+  }
+  if (validator.validateMinLength(form.about.value, 50)) {
+    message = 'About me section must have at least 50 characters.';
+    alert(message);
+  }
+  if (message) {
+    event.preventDefault();
+  }
 }
 
-var validation = new Validation();
-validation.checkValue('form'); 
+window.addEventListener('load', function() {
+  var validation = new Validation();
+  validation.init('form'); 
+});
