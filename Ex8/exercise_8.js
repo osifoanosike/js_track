a175358d593a40402f557e50b294e39c4394a5cd
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-02-18 11:10:22 +0100

    Add init() method which calls openUrl() method.

--- Ex8/exercise_8.js
+++ Ex8/exercise_8.js
@@ -1,18 +1,22 @@
 function UrlOpener() {
 }
 
-UrlOpener.prototype.openUrl = function() {
-  window.addEventListener('load', function() {
-    do {
-      var address = prompt('Please enter the web address.');
-    } while (validator.empty(address));
-
-    var windowFeatures = 'menubar=no,location=no,status=no,scrollbars=0,width=400,height=450';
-    var url = 'http://' + address;
-    alert('You would like to visit ' + url);
-    window.open(url, '_blank', windowFeatures);
-  }, false);
+UrlOpener.prototype.init = function() {
+  this.openUrl();
 }
 
-var opener = new UrlOpener();
-opener.openUrl();
+UrlOpener.prototype.openUrl = function() {
+  do {
+    var address = prompt('Please enter the web address.');
+  } while (validator.empty(address));
+
+  var windowFeatures = 'menubar=no,location=no,status=no,scrollbars=0,width=400,height=450';
+  var url = 'http://' + address;
+  alert('You would like to visit ' + url);
+  window.open(url, '_blank', windowFeatures);
+}
+
+window.addEventListener('load', function() {
+  var opener = new UrlOpener();
+  opener.init();
+});
