ebb16249419d7798d85176c9d7679710ed37c844
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-02-18 10:43:24 +0100

    Add an init() method.

--- Ex6/exercise_6.js
+++ Ex6/exercise_6.js
@@ -1,30 +1,34 @@
 function Validation() {
 }
 
-Validation.prototype.checkValue = function(formName) {
+Validation.prototype.init = function(formName) {
+  var that = this;
   var form = document.forms[formName];
   form.addEventListener('submit', function() {
-    var details = document.querySelectorAll('.details');
-    var message = "";
+    that.checkValue(this)
+  });
+}
 
-    for (var i = 0; i < details.length; i++) {
-      if (validator.empty(details[i].value)) {
-        var labelText = details[i].previousElementSibling.textContent;
-        message = labelText + ' cannot be empty.';
-        alert(message);
-      }
-    }
-    if (validator.validateMinLength(form.about.value, 50)) {
-      message = 'About Me must have at least 50 characters.';
+Validation.prototype.checkValue = function(form) {
+  var details = document.querySelectorAll('.details');
+  var message = "";
+  for (var i = 0; i < details.length; i++) {
+    if (validator.empty(details[i].value)) {
+      var labelText = details[i].previousElementSibling.textContent;
+      message = labelText + ' cannot be empty.';
       alert(message);
     }
-    if (message) {
-      event.preventDefault();
-    }
-  });
+  }
+  if (validator.validateMinLength(form.about.value, 50)) {
+    message = 'About Me must have at least 50 characters.';
+    alert(message);
+  }
+  if (message) {
+    event.preventDefault();
+  }
 }
 
 window.addEventListener('load', function() {
   var validation = new Validation();
-  validation.checkValue('form');
+  validation.init('form'); 
 });
