2c5be92e19f4c4c67f1ae4e4cb56c5f1551b4624
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-02-18 10:59:29 +0100

    Add init() method. Move load event listener on windows object out of the getName() method.

--- Ex7/exercise_7.js
+++ Ex7/exercise_7.js
@@ -1,20 +1,24 @@
 function Account() {
 }
 
-Account.prototype.getName = function() {
-  window.addEventListener('load', function() {
-    do {
-      var firstName = prompt('What is your first name');
-    } while (validator.empty(firstName))
-    do {
-      var lastName = prompt('What is your last name');
-    } while (validator.empty(lastName))
-
-    var message = 'Hello ' + firstName + ' ' + lastName + '.';
-    alert(message);
-    document.write('<strong>' + message + '</strong>');
-  });  
+Account.prototype.init = function() {
+  this.getName();
 }
 
-var owner = new Account();
-owner.getName();
+Account.prototype.getName = function() {
+  do {
+    var firstName = prompt('What is your first name');
+  } while (validator.empty(firstName))
+  do {
+    var lastName = prompt('What is your last name');
+  } while (validator.empty(lastName))
+
+  var message = 'Hello ' + firstName + ' ' + lastName + '.';
+  alert(message);
+  document.write('<strong>' + message + '</strong>');
+}
+
+window.addEventListener('load', function() {
+  var owner = new Account();
+  owner.init();
+});
